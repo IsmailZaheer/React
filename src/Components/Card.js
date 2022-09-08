e7808e409333a834a22ddd2ea097689bd6179923
ismail <ismailsheikh021@gmail.com>
2022-09-09 00:36:13 +0500

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from "react"
+const Card = ({name,email,id}) =>{
+    return(
+          <div className="bg-light-pink dib br4 pa3 ma2 grow bw2 shadow-5">
+            <img alt ='robots' src={`https://robohash.org/${id} width={100} height={100} `}/>
+            <div>
+                <h2>
+                    {name}
+                </h2>
+                <p>
+                    {email}
+                </p>
+            </div>
+          </div>
+    );
+}
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,83 @@
+export const robots =[
+    {
+        id :  1,
+        name : 'ISMAIL ZAHEER ',
+        username: ' ISMIII',
+        email: 'ismailsheikh021@gmail.com'
+
+
+    },
+    {
+        id : 2,
+        name : 'JANE DOE',
+        username: ' DOE',
+        email: 'isnahk@yahoo.com '
+
+
+    },
+    {
+        id : 3 ,
+        name : ' Walter White',
+        username: 'Halt',
+        email: 'walterwhite@aol.com'
+
+
+    },
+    {
+        id :  4,
+        name : ' Gustavo Fring',
+        username: 'Gus',
+        email: 'lospollos@gmail.com '
+
+
+    },
+    {
+        id :  5,
+        name : ' Mike',
+        username: ' mikey',
+        email: ' mikecop@aol.com'
+
+
+    },
+    {
+        id : 6,
+        name : 'Hank',
+        username: ' hank',
+        email: ' hank234@yahoo.com'
+
+
+    },
+    {
+        id :  7,
+        name : ' Rebecca',
+        username: ' Red',
+        email: 'REBRB222@outlook.com '
+
+
+    },
+    {
+        id : 8,
+        name : ' Andres',
+        username: ' Berlin',
+        email: 'bb@yahoo.com '
+
+
+    }, {
+        id :  9,
+        name : ' Rebecca',
+        username: ' Red',
+        email: 'REBRB222@outlook.com '
+
+
+    },
+    {
+        id : 10,
+        name : ' Andres',
+        username: ' Berlin',
+        email: 'bb@yahoo.com '
+
+
+    }
+
+
+]
